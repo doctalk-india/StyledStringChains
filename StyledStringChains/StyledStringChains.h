5d5f5a0278511efea9a1bae57193c7298be0c91f
Sourav Chandra <saurav.chandra1992@live.com>
2019-03-17 19:28:31 +0530

    Initial Commit

---
+++ StyledStringChains/StyledStringChains.h
@@ -0,0 +1,19 @@
+//
+//  StyledStringChains.h
+//  StyledStringChains
+//
+//  Created by Sourav Chandra on 17/03/19.
+//  Copyright © 2019 Pulse Care. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StyledStringChains.
+FOUNDATION_EXPORT double StyledStringChainsVersionNumber;
+
+//! Project version string for StyledStringChains.
+FOUNDATION_EXPORT const unsigned char StyledStringChainsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StyledStringChains/PublicHeader.h>
+
+
